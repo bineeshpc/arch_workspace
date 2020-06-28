d3088cc72cf7f8926d67c099797a5f2eb1c89012
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2020-06-28 13:35:53 +0530

    added support for multiple os not only arch

--- system_setup/install.py
+++ system_setup/install.py
@@ -4,13 +4,16 @@ import os
 import shlex
 import logging
 import conffile
+import argparse
 
-logging.basicConfig(filename='/tmp/install.log',level=logging.DEBUG)
+filename = "/tmp/install.log"
+print(f"check output in {filename}")
+logging.basicConfig(filename=filename,level=logging.DEBUG)
 home = os.environ["HOME"]
 
 def completed_process_to_string(completed_process):
     s = []
-    
+
     s.append(completed_process.args)
     s.append(str(completed_process.returncode))
     s.append(completed_process.stdout.decode("utf-8"))
@@ -55,6 +58,42 @@ class Job(ABC):
         pass
 
 
+class Arch:
+    def __init__(self, name):
+        self.name = name
+        self.install_cmd = "pacman --noconfirm -S "
+        self.configure_cmd = "pacman -Qi "
+
+    def get_install_cmd(self):
+        return self.install_cmd
+
+    def get_configure_cmd(self):
+        return self.configure_cmd
+
+class Ubuntu:
+    def __init__(self, name):
+        self.name = name
+        self.install_cmd = f"apt-get update && \
+        DEBIAN_FRONTEND=noninteractive apt-get --yes --allow-downgrades install "
+        self.configure_cmd = "dpkg -s "
+
+    def get_install_cmd(self):
+        return self.install_cmd
+
+    def get_configure_cmd(self):
+        return self.configure_cmd
+
+class OS:
+    def __init__(self, name):
+        self.name = name
+
+    def get_os(self):
+        if self.name == "arch":
+            return Arch(self.name)
+        if self.name == "ubuntu":
+            return Ubuntu(self.name)
+
+
 class Dummy(Job):
 
     def __init__(self):
@@ -72,21 +111,21 @@ class Dummy(Job):
 
 
 class Editors(Job):
-    def __init__(self):
+    def __init__(self, os):
         self.editors = ["emacs", "vim"]
+        self.os = OS(name=os).get_os()
         super().__init__()
 
     def install(self):
         for editor in self.editors:
-            run_as_user("root", f"pacman --noconfirm -S {editor}")
+            run_as_user("root", f"{self.os.get_install_cmd()} {editor}")
 
     def configure(self):
         """
         Check whether installed
         """
         for editor in self.editors:
-            run(f"pacman -Qi {editor}")
-
+            run(f"{self.os.get_configure_cmd()} {editor}")
 
 class Git(Job):
     def __init__(self):
@@ -125,7 +164,7 @@ class Codeserver(Job):
     def install(self):
         run("curl -fsSL https://code-server.dev/install.sh | sh")
         run("""echo 'export  PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc""")
-    
+
     def configure(self):
         pass
 
@@ -187,12 +226,13 @@ class Swap(Job):
 
 class Screen(Job):
 
-    def __init__(self):
+    def __init__(self, os):
+        self.os = OS(name=os).get_os()
         super().__init__()
 
     def install(self):
-        run_as_user("root", "pacman --noconfirm -S screen")
-        
+        run_as_user("root", f"{self.os.get_install_cmd()} screen")
+
     def configure(self):
         dir_path = os.path.dirname(os.path.abspath(__file__))
         run(f"cp {dir_path}/screenrc ~/.screenrc")
@@ -205,7 +245,7 @@ class Java(Job):
 
     def install(self):
         pass
-        
+
     def configure(self):
         run("echo 'export JAVA_HOME=/usr/lib/jvm/default/' >> ~/.bashrc")
 
@@ -219,7 +259,7 @@ class Hadoop2(Job):
 
 
     def install(self):
-        if not os.path.exists(self.install_path):            
+        if not os.path.exists(self.install_path):
             run("mkdir -p downloads")
             run("cd downloads && wget https://mirrors.estointernet.in/apache/hadoop/common/hadoop-2.10.0/hadoop-2.10.0.tar.gz")
             run("mkdir -p programfiles")
@@ -237,7 +277,7 @@ export HADOOP_INSTALL=$HADOOP_HOME
 export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
 export PATH=${JAVA_HOME}/bin:${PATH}"""
         self.cf = conffile.Conf(self.conf, f"{home}/.bashrc")
-        
+
         if self.add_conf:
             otherhadoop = Hadoop3(add_conf=False)
             if otherhadoop.cf.is_conf_present():
@@ -257,7 +297,7 @@ class Hadoop3(Job):
 
 
     def install(self):
-        if not os.path.exists(self.install_path):            
+        if not os.path.exists(self.install_path):
             run("mkdir -p downloads")
             run("cd downloads && wget http://mirrors.estointernet.in/apache/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz")
             run("mkdir -p programfiles")
@@ -275,16 +315,15 @@ export HADOOP_INSTALL=$HADOOP_HOME
 export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
 export PATH=${JAVA_HOME}/bin:${PATH}"""
         self.cf = conffile.Conf(self.conf, f"{home}/.bashrc")
-        
 
-        if self.add_conf:        
+
+        if self.add_conf:
             otherhadoop = Hadoop2(add_conf=False)
             if otherhadoop.cf.is_conf_present():
                 otherhadoop.cf.delete()
 
             if not self.cf.is_conf_present():
                 self.cf.add()
-
 
 
 class Spark2(Job):
@@ -312,14 +351,14 @@ export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH
 export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.7-src.zip:$PYTHONPATH"""
         self.cf = conffile.Conf(self.conf, f"{home}/.bashrc")
 
-        if self.add_conf:        
+        if self.add_conf:
             other = Spark3(add_conf=False)
             if other.cf.is_conf_present():
                 other.cf.delete()
 
             if not self.cf.is_conf_present():
                 self.cf.add()
-       
+
 
 class Spark3(Job):
 
@@ -346,27 +385,33 @@ export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH
 export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.8.1-src.zip:$PYTHONPATH"""
         self.cf = conffile.Conf(self.conf, f"{home}/.bashrc")
 
-        if self.add_conf:        
+        if self.add_conf:
             other = Spark2(add_conf=False)
             if other.cf.is_conf_present():
                 other.cf.delete()
 
             if not self.cf.is_conf_present():
                 self.cf.add()
-       
-        
+
+
+def parse_cmdline():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("os", type=str)
+    return parser.parse_args()
+
 def main():
-    Dummy()
-    # Editors()
+    args = parse_cmdline()
+    # Dummy()
+    Editors(args.os)
     # Git()
     # Nvidia()
-    # Screen()
+    Screen(args.os)
     # Java()
-    Hadoop2()
-    Spark2()
+    # Hadoop2()
+    # Spark2()
     # Hadoop3()
     # Spark3()
-    pass
+
 
 
 if __name__ == "__main__":
